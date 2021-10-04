f1a25cdb4a3d3f2d165c7831dbc944caebadbce0
Patrick Rudolph <patrick.rudolph@9elements.com>
2021-10-04 12:58:50 +0200

    Main: Parse arguments
    Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,16 +1,44 @@
+
+#include <boost/program_options.hpp>
 #include <iostream>
-#include <ctime>
 #include "Config.hpp" 
 #include "SignalProvider.hpp" 
 #include "ACPIStates.hpp" 
-
 #include "StateMachine.hpp" 
 
 using namespace std;
- 
-int main(void) {
-  Config cfg = LoadConfig("config");
+using namespace boost::program_options;
+
+int main(int argc, const char *argv[]) {
+  Config cfg;
   
+  try
+  {
+    options_description desc{"Options"};
+    desc.add_options()
+      ("help,h", "Help screen")
+      ("config", value<string>()->default_value(""), "Path to configuration file/folder.");
+
+    variables_map vm;
+    store(parse_command_line(argc, argv, desc), vm);
+    notify(vm);
+
+    if (vm.count("help")) {
+      std::cout << desc << '\n';
+      return 1;
+    }
+    if (!vm.count("config") || vm["config"].as<string>() == "") {
+      std::cout << desc << '\n';
+      return 1;
+    }
+
+      cfg = LoadConfig(vm["config"].as<string>());
+  }
+  catch (const error &ex)
+  {
+    std::cerr << ex.what() << '\n';
+  }
+
   SignalProvider signalprovider(cfg);
   ACPIStates states(cfg, signalprovider);
   StateMachine sm(cfg, signalprovider);
