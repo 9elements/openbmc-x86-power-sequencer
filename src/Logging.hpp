2549e646c39526d77368048b1902a6e14b2e93db
Patrick Rudolph <patrick.rudolph@9elements.com>
2021-11-24 10:37:20 +0100

    src/Logging: Fix WITH_PHOSPHOR_LOGGING
    Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>

--- src/Logging.hpp
+++ src/Logging.hpp
@@ -2,13 +2,28 @@
 
 extern int _loglevel;
 #ifdef WITH_PHOSPHOR_LOGGING
+#include <phosphor-logging/log.hpp>
+
 #define LOGDEBUG(x)                                                            \
     if (_loglevel > 1)                                                         \
-    phosphor::logging::log<phosphor::logging::level::DEBUG>(x)
+    {                                                                          \
+        phosphor::logging::log<phosphor::logging::level::DEBUG>(x);            \
+        std::cout << "DBG : " << x << std::endl;                               \
+    }
+
 #define LOGINFO(x)                                                             \
     if (_loglevel > 0)                                                         \
-    phosphor::logging::log<phosphor::logging::level::INFO>(x)
-#define LOGERR(x) phosphor::logging::log<phosphor::logging::level::ERR>(x)
+    {                                                                          \
+        phosphor::logging::log<phosphor::logging::level::INFO>(x);             \
+        std::cout << "INFO : " << x << std::endl;                              \
+    }
+
+#define LOGERR(x)                                                              \
+    {                                                                          \
+        phosphor::logging::log<phosphor::logging::level::ERR>(x);              \
+        std::cerr << "ERR : " << x << std::endl;                               \
+    }
+
 #else
 #define LOGDEBUG(x)                                                            \
     if (_loglevel > 1)                                                         \
