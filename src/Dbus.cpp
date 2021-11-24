40e1b4d90d21056f6797a911505243b8ad4ff28d
Patrick Rudolph <patrick.rudolph@9elements.com>
2021-11-24 09:40:00 +0100

    src/Dbus: Add basic dbus interface
    Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>

---
+++ src/Dbus.cpp
@@ -0,0 +1,150 @@
+
+#include "Dbus.hpp"
+
+static std::string hostDbusName = "xyz.openbmc_project.State.Host";
+static std::string chassisDbusName = "xyz.openbmc_project.State.Chassis";
+
+static constexpr std::string_view getHostState(const PowerState state)
+{
+    switch (state)
+    {
+        case PowerState::on:
+        case PowerState::gracefulTransitionToOff:
+        case PowerState::gracefulTransitionToCycleOff:
+            return "xyz.openbmc_project.State.Host.HostState.Running";
+            break;
+        case PowerState::waitForPSPowerOK:
+        case PowerState::waitForSIOPowerGood:
+        case PowerState::off:
+        case PowerState::transitionToOff:
+        case PowerState::transitionToCycleOff:
+        case PowerState::cycleOff:
+        case PowerState::checkForWarmReset:
+            return "xyz.openbmc_project.State.Host.HostState.Off";
+            break;
+        default:
+            return "";
+            break;
+    }
+};
+
+static constexpr std::string_view getChassisState(const PowerState state)
+{
+    switch (state)
+    {
+        case PowerState::on:
+        case PowerState::transitionToOff:
+        case PowerState::gracefulTransitionToOff:
+        case PowerState::transitionToCycleOff:
+        case PowerState::gracefulTransitionToCycleOff:
+        case PowerState::checkForWarmReset:
+            return "xyz.openbmc_project.State.Chassis.PowerState.On";
+            break;
+        case PowerState::waitForPSPowerOK:
+        case PowerState::waitForSIOPowerGood:
+        case PowerState::off:
+        case PowerState::cycleOff:
+            return "xyz.openbmc_project.State.Chassis.PowerState.Off";
+            break;
+        default:
+            return "";
+            break;
+    }
+};
+
+void Dbus::SetPowerState(const PowerState state)
+{
+#ifdef WITH_SDBUSPLUSPLUS
+    this->hostIface->set_property("CurrentHostState",
+                                  std::string(getHostState(state)));
+
+    this->chassisIface->set_property("CurrentPowerState",
+                                     std::string(getChassisState(state)));
+    // this->chassisIface->set_property("LastStateChangeTime",
+    // getCurrentTimeMs());
+#endif
+}
+
+void Dbus::RegisterRequestedHostTransition(
+    const std::function<bool(const std::string& requested, std::string& resp)>&
+        handler)
+{
+#ifdef WITH_SDBUSPLUSPLUS
+
+    this->hostIface->register_property(
+        "RequestedHostTransition",
+        std::string("xyz.openbmc_project.State.Host.Transition.Off"), handler);
+#endif
+}
+
+void Dbus::RegisterRequestedPowerTransition(
+    const std::function<bool(const std::string& requested, std::string& resp)>&
+        handler)
+{
+#ifdef WITH_SDBUSPLUSPLUS
+    this->chassisIface->register_property(
+        "RequestedPowerTransition",
+        std::string("xyz.openbmc_project.State.Chassis.Transition.Off"),
+        handler);
+
+#endif
+}
+
+void Dbus::Initialize(void)
+{
+#ifdef WITH_SDBUSPLUSPLUS
+    this->hostIface->initialize();
+    this->chassisIface->initialize();
+#endif
+}
+
+Dbus::Dbus(Config& cfg, boost::asio::io_service io) :
+#ifdef WITH_SDBUSPLUSPLUS
+
+    conn{std::make_shared<sdbusplus::asio::connection>(io)}, hostServer{conn},
+    chassisServer
+{
+    conn
+}
+
+#endif
+{
+    string node = "0";
+#ifdef WITH_SDBUSPLUSPLUS
+
+    if (node == "0")
+    {
+        // Request all the dbus names
+        conn->request_name(hostDbusName.c_str());
+        conn->request_name(chassisDbusName.c_str());
+    }
+
+    hostDbusName += node;
+    chassisDbusName += node;
+
+    // Request all the dbus names
+    conn->request_name(hostDbusName.c_str());
+    conn->request_name(chassisDbusName.c_str());
+
+    // Power Control Interface
+    this->hostIface =
+        hostServer.add_interface("/xyz/openbmc_project/state/host" + node,
+                                 "xyz.openbmc_project.State.Host");
+
+    this->hostIface->register_property(
+        "CurrentHostState", std::string(getHostState(PowerState::off)));
+
+    // Chassis Control Interface
+    this->chassisIface =
+        chassisServer.add_interface("/xyz/openbmc_project/state/chassis" + node,
+                                    "xyz.openbmc_project.State.Chassis");
+
+    this->chassisIface->register_property(
+        "CurrentPowerState", std::string(getChassisState(PowerState::off)));
+    // this->chassisIface->register_property("LastStateChangeTime",
+    //                                     getCurrentTimeMs());
+#endif
+}
+
+Dbus::~Dbus()
+{}
---
+++ src/Dbus.hpp
@@ -0,0 +1,93 @@
+#pragma once
+
+#include "Config.hpp"
+
+#include <boost/asio.hpp>
+
+#ifdef WITH_SDBUSPLUSPLUS
+#include <sdbusplus/asio/object_server.hpp>
+#endif
+
+using namespace std;
+
+enum class PowerState
+{
+    on,
+    waitForPSPowerOK,
+    waitForSIOPowerGood,
+    off,
+    transitionToOff,
+    gracefulTransitionToOff,
+    cycleOff,
+    transitionToCycleOff,
+    gracefulTransitionToCycleOff,
+    checkForWarmReset,
+};
+
+static std::string getPowerStateName(PowerState state)
+{
+    switch (state)
+    {
+        case PowerState::on:
+            return "On";
+            break;
+        case PowerState::waitForPSPowerOK:
+            return "Wait for Power Supply Power OK";
+            break;
+        case PowerState::waitForSIOPowerGood:
+            return "Wait for SIO Power Good";
+            break;
+        case PowerState::off:
+            return "Off";
+            break;
+        case PowerState::transitionToOff:
+            return "Transition to Off";
+            break;
+        case PowerState::gracefulTransitionToOff:
+            return "Graceful Transition to Off";
+            break;
+        case PowerState::cycleOff:
+            return "Power Cycle Off";
+            break;
+        case PowerState::transitionToCycleOff:
+            return "Transition to Power Cycle Off";
+            break;
+        case PowerState::gracefulTransitionToCycleOff:
+            return "Graceful Transition to Power Cycle Off";
+            break;
+        case PowerState::checkForWarmReset:
+            return "Check for Warm Reset";
+            break;
+        default:
+            return "unknown state: " + std::to_string(static_cast<int>(state));
+            break;
+    }
+}
+
+// The Dbus class handles the DBUS interface
+class Dbus
+{
+  public:
+    Dbus(Config& cfg, boost::asio::io_service io);
+    ~Dbus();
+    void SetPowerState(const PowerState);
+    void RegisterRequestedHostTransition(
+        const std::function<bool(const std::string& requested,
+                                 std::string& resp)>& handler);
+
+    void RegisterRequestedPowerTransition(
+        const std::function<bool(const std::string& requested,
+                                 std::string& resp)>& handler);
+    void Initialize(void);
+
+  private:
+#ifdef WITH_SDBUSPLUSPLUS
+    std::shared_ptr<sdbusplus::asio::connection> conn;
+
+    sdbusplus::asio::object_server hostServer;
+    sdbusplus::asio::object_server chassisServer;
+
+    std::shared_ptr<sdbusplus::asio::dbus_interface> hostIface;
+    std::shared_ptr<sdbusplus::asio::dbus_interface> chassisIface;
+#endif
+};
